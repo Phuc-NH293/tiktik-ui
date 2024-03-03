fdd4755d36ac53b419515ef65b7277b7529843cc
Phuc-NH293 <122101849+Phuc-NH293@users.noreply.github.com>
2024-03-03 22:45:46 +0700

    ok

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 /* eslint-disable no-unused-vars */
+import { Fragment } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { publicRoutes } from './routes';
 import { DefaultLayout } from './components/Layout';
@@ -8,7 +9,7 @@ function App() {
             <div className="App">
                 <Routes>
                     {publicRoutes.map((route, index) => {
-                        const Layout = route.layout || DefaultLayout;
+                        const Layout = route.layout === null ? Fragment : DefaultLayout;
                         const Page = route.component;
                         return (
                             <Route
