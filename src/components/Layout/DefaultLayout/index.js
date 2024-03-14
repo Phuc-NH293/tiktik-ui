f35b30caf2cf4889d33dc1f84a575d6e93e0c034
Phuc-NH293 <122101849+Phuc-NH293@users.noreply.github.com>
2024-03-14 19:19:19 +0700

    khunglayout_tiktok

--- src/components/Layout/DefaultLayout/index.js
+++ src/components/Layout/DefaultLayout/index.js
@@ -1,13 +1,17 @@
 import Sidebar from './Sidebar';
+import classNames from 'classnames/bind';
+import styles from './DeafaultLayout.model.scss';
 import Header from '../components/Header';
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
     return (
-        <div>
+        <div className={cx('wrapper')}>
             <Header />
-            <div className="container">
+            <div className={cx('container')}>
                 <Sidebar />
+
+                <div className={cx('content')}>{children}</div>
             </div>
-            <div className="content">{children}</div>
         </div>
     );
 }
